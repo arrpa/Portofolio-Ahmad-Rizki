4bb2b6f2f5aac1a8d948210c5fd8c0c0cef49d2e
arrpa <ahmadbrinegt@gmail.com>
2024-02-05 00:12:40 +0800

    Portofolio project

---
+++ src/App.jsx
@@ -0,0 +1,348 @@
+import "./App.css";
+import Profile from "./assets/Stares.webp";
+import Project1 from "./assets/projectP.png";
+import Project2 from "./assets/projectT.png";
+import Project3 from "./assets/project3.png";
+import Project4 from "./assets/project4.png";
+import Facebook from "./assets/facebook.svg";
+import Github from "./assets/Github.png";
+import Instagram from "./assets/instagram.svg";
+import ArrowDown from "./assets/arrow-down.svg";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [scrolling, setScrolling] = useState(false);
+
+  const onPageScroll = () => {
+    if(window.pageYOffset > 200) {
+      setScrolling(true)
+    } else {
+      setScrolling(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onPageScroll)
+    return() => {
+      window.removeEventListener("scroll", onPageScroll)
+    }
+  }, [])
+
+  return (
+    <div className="max-w-4xl m-auto relative">
+      <header className={`${scrolling ? 'border-b border-gray-900' : ''}  fixed left-0 right-0 top-0 z-20`} id="home">
+        <div className="container m-auto px-4 py-6 max-w-4xl bg-black">
+          <div className="flex flex-col gap-4 sm:flex-row justify-between items-center">
+            <div>
+              <h1 className="font-bold text-2xl">Arrpa Portfolio</h1>
+            </div>
+            <div>
+              <ul className="flex gap-4">
+                <li>
+                  <a href="#projects" className="text-gray-400 hover:text-white cursor-pointer">
+                    Projects
+                  </a>
+                </li>
+                <li>
+                  <a href="#technologies" className="text-gray-400 hover:text-white cursor-pointer">
+                    Technologies
+                  </a>
+                </li>
+                <li>
+                  <a href="#aboutme" className="text-gray-400 hover:text-white cursor-pointer">
+                    About me
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </header>
+      <main className="relative mt-28">
+        {/* Intro/Banner section */}
+        <section>
+          <div className="container m-auto px-4 pt-12 pb-12 sm:pt-20 flex flex-col sm:flex-row gap-6 text-center sm:text-left">
+            <div>
+              <h2 className="font-bold text-4xl">Hello, I'm Ahmad Rizki,</h2>
+              <div>
+                <h2 className="font-bold text-4xl mt-1 gradiant-text">student developer</h2>
+              </div>
+              <div>
+                <p className="mt-4 text-gray-400">
+                  Siswa SMKTI Airlangga yang sedang mempelajari bidang web development
+                </p>
+                <button className="px-8 shadow-gray-500 shadow-md py-5 mt-5 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                  Subscribe!
+                </button>
+              </div>
+            </div>
+            <div className="relative">
+              <div className="after:bg-[url('./large-long.png')] after:bg-contain after:block after:bg-no-repeat after:w-[420px] after:h-[320px] after:absolute after:top-0 after:-left-20 sm:after:-left-40 before:bg-[url('./small.png')] before:bg-contain before:block before:bg-no-repeat before:w-[220px] before:h-[220px] before:absolute before:bottom-0 before:-right-10">
+                <img src={Profile} className="relative z-10 w-[280px] m-auto sm:w-[600px] rounded-e-full rounded-s-full rounded-b-full	rounded-t-full" />
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* Projects section */}
+        <section id="projects">
+          <div className="container m-auto px-4 sm:py-12">
+            <h2 className="text-2xl font-semibold">Projects</h2>
+            <div className="flex flex-col sm:flex-row gap-10 mt-11">
+              <div className="border border-gray-500 rounded-md p-5 flex-1">
+                <img src={Project1} className="w-full h-auto" />
+                <h3 className="text-2xl font-semibold mt-8">
+                  Portofolio website
+                </h3>
+                <p className="text-gray-400 text-sm mt-2">
+                  Reactjs, jsx, css
+                </p>
+                <div className="flex mt-12 gap-2">
+                  {/* <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                    Live preview
+                  </button> */}
+                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                    <a href="https://github.com/arrpa/Portofolio-Ahmad-Rizki">
+                    Checkout github
+                    </a>
+                  </button>
+                </div>
+              </div>
+              <div className="border border-gray-500 rounded-md p-5 flex-1">
+                <img src={Project2} className="w-full h-auto" />
+                <h3 className="text-2xl font-semibold mt-8">
+                  TicTacToe demo
+                </h3>
+                <p className="text-gray-400 text-sm mt-2">
+                  Java with Netbeans 18
+                </p>
+                <div className="flex gap-2 mt-12">
+                  {/* <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                    Live preview
+                  </button> */}
+                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                    <a href="https://github.com/arrpa/Tictactoe-demo">
+                    Checkout github
+                    </a>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col sm:flex-row  gap-10 mt-11">
+              {/* <div className="border border-gray-500 rounded-md p-5 flex-1">
+                <img src={Project3} className="w-full h-auto" />
+                <h3 className="text-2xl font-semibold mt-8">
+                  Landing page for front-end developer
+                </h3>
+                <p className="text-gray-400 text-sm mt-2">
+                  Responsive HTML/CSS layout for landing page for front-end
+                  developer. HTML5, CSS3 (SCSS)
+                </p>
+                <div className="flex gap-4 mt-12">
+                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                    Live preview
+                  </button>
+                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                    Checkout github
+                  </button>
+                </div>
+              </div> */}
+              {/* <div className="border border-gray-500 rounded-md p-5 flex-1">
+                <img src={Project4} className="w-full h-auto" />
+                <h3 className="text-2xl font-semibold mt-8">
+                  Website redesign for The Venus project
+                </h3>
+                <p className="text-gray-400 text-sm mt-2">
+                  Responsive HTML/CSS layout for The Venus project. HTML5, CSS3
+                  (SCSS)
+                </p>
+                <div className="flex gap-2 mt-12">
+                  <button className="flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                    Live preview
+                  </button>
+                  <button className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500">
+                    Checkout github
+                  </button>
+                </div>
+              </div> */}
+            </div>
+          </div>
+        </section>
+        {/* Technoglies section */}
+        <section className="py-10" id="technologies">
+          <div className="container m-auto px-4">
+            <h2 className="text-2xl font-semibold">Technologies</h2>
+            <div className="mt-14">
+              <div>
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">HTML</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[16%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">CSS, Sass & Bootstrap</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[5%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">
+                    JavaScript
+                  </h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[10%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">UI design in Figma</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[13%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">Python</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[20%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">React</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[8%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+              <div className="mt-8">
+                <div className="flex justify-between items-center">
+                  <h2 className="font-semibold">Java</h2>
+                  <p className="text-gray-500">Advanced</p>
+                </div>
+                <span className="w-[3%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
+              </div>
+            </div>
+          </div>
+        </section>
+        {/* Additional skills section */}
+        <section>
+          <div className="container m-auto px-4 py-14">
+            <h2 className="text-2xl font-semibold">
+              Additional technologies and skills
+            </h2>
+            <div className="flex flex-col sm:flex-row gap-4 sm:gap-28 mt-12 w-[80%]">
+              <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                  Git
+                </p>
+              </div>
+              {/* <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                  Java
+                </p>
+              </div> */}
+              <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                  Teamwork
+                </p>
+              </div>
+              <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                  Quick learning
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-col sm:flex-row gap-4 sm:gap-36 mt-4 sm:mt-6 w-[80%]">
+              <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                  Engagement
+                </p>
+              </div>
+              <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                  2 Language
+                </p>
+              </div>
+              <div>
+                <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
+                  C#
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="py-8" id="aboutme">
+          <div className="container m-auto px-4">
+            <h2 className="text-2xl font-semibold">About me</h2>
+            <div className="mt-12 relative before:absolute before:top-0 before:left-16 before:rounded-full before:bottom-10 sm:before:bottom-2 before:w-1 before:bg-white">
+              <div className="pl-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+                <h3 className="absolute left-0 text-lg font-semibold">2013</h3>
+                <p>
+                  Masuk SD Muhammadiyah2 Sangatta utara dan pindah ke SD Normal Islam saat kelas 5 semester 2
+                </p>
+              </div>
+              <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+                <h3 className="absolute left-0 text-lg font-semibold">2018</h3>
+                <p>
+                  Setelah lulus SD saya memasuki MTSN Model Samarinda
+                </p>
+              </div>
+              <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+                <h3 className="absolute left-0 text-lg font-semibold">2021</h3>
+                <p>
+                  Setelah lulus dari MTSN Model Samarinda saya memasuki SMKTI Airlangga Samarinda dan memilih jurusan PPLG karena saya ertarik dengan ilmu komputer
+                </p>
+              </div>
+              {/* <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
+                <h3 className="absolute left-0 text-lg font-semibold">2021-Present</h3>
+                <p>
+                  Worked out in which field I want to build a developer's career
+                  and started to learn Frontend Development.
+                </p>
+              </div> */}
+            </div>
+          </div>
+        </section>
+      </main>
+      <footer>
+        <div className="container m-auto flex justify-between px-4 py-6">
+          <div>
+            <p className="text-gray-300 text-sm">Copyright @ 2023</p>
+          </div>
+          <div>
+            <ul className="flex gap-4">
+              <li>
+                <a href="https://web.facebook.com/profile.php?id=100004672639417">
+                  <img src={Facebook} className="w-5" />
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com/arrpa">
+                  <img src={Github} className="w-5" />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/arrpa06/">
+                  <img src={Instagram} className="w-5" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+      {
+        scrolling && (
+          <button className="fixed block right-8 bottom-0 w-24" onClick={() => {
+            window.scrollTo(0,0);
+          }}>
+            <img src={ArrowDown} />
+          </button>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
